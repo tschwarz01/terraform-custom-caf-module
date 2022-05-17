6d64686611bfc5db619bbc861be624c7e82b0cf9
Thomas Schwarz <thosch@microsoft.com>
2022-05-17 17:23:08 -0500

    upgrade sql failover module to azurerm_mssql_failover_group

--- databases/mssql_server/failover_group/main.tf
+++ databases/mssql_server/failover_group/main.tf
@@ -9,6 +9,12 @@ terraform {
 
 locals {
   databases = [
-    for key, value in var.settings.databases : var.databases[try(value.lz_key, var.client_config.landingzone_key)][value.database_key].id
+    for key, value in var.settings.databases : var.databases[value.database_key].id
   ]
+
+  module_tag = {
+    "module" = basename(abspath(path.module))
+  }
+
+  tags = merge(local.module_tag, try(var.settings.tags, null), var.base_tags)
 }
