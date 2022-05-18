2b7a2bde3573ca763c3dd193ecefac9b81dd4d89
Thomas Schwarz <thosch@microsoft.com>
2022-05-18 13:51:45 -0500

    tweak shared shir delay

--- data_factory/data_factory_integration_runtime_shared_self_hosted/module.tf
+++ data_factory/data_factory_integration_runtime_shared_self_hosted/module.tf
@@ -16,7 +16,7 @@ resource "azurerm_role_assignment" "target" {
 
 resource "time_sleep" "delay" {
   depends_on      = [azurerm_role_assignment.target]
-  create_duration = "300s"
+  create_duration = "500s"
 }
 
 resource "azurerm_data_factory_integration_runtime_self_hosted" "dfirsh" {
